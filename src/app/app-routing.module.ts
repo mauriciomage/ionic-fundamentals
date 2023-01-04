cbcc7b8b708a5eac0bd95c869335c68007348377
Mauricio <mauriciomage@gmail.com>
2023-01-04 09:57:01 +0100

    init app and add list item

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./contact/contact.module').then( m => m.ContactPageModule)
+  },
 ];
 @NgModule({
   imports: [
